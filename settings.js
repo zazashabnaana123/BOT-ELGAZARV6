11be3a0f271f1aa4c39a5943be947c22e2eaa7c5
zazashabnaana123 <123482274+zazashabnaana123@users.noreply.github.com>
2023-05-01 14:34:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
+global.owner = ['972593819563'] //ur owner number
+global.ownername = "" //ur owner name
 global.ytname = "YT: ELGAZAR" //ur yt chanel name
 global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
+global.ownernomer = "972593819563" //ur number
+global.premium = ['972593819563'] //ur premium number
+global.botname = '𝐇𝐨𝐭𝐚𝐫𝐨 𓆩🔮𓆪 𝐁𝐨𝐭' //ur bot name
 global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "𝐎𝐬𝐚𝐤𝐚 𓆩🔮𓆪 𝐁𝐨𝐭" //ur sticker watermark packname
+global.author = "𝐇𝐨𝐭𝐚𝐫𝐨 𓆩🔮𓆪 𝐁𝐨𝐭" //ur sticker watermark author
+global.wm = "𝐇𝐨𝐭𝐚𝐫𝐨 𓆩🔮𓆪 𝐁𝐨𝐭" //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -41,14 +41,14 @@ global.sp = ''
 //messages
 global.mess = {
     success: 'تم يروحي✨',
-    admin: '『 الامر دا لادمن بس! 』',
-    botAdmin: '『 ارفع البوت ادمن الاول 』',
+    admin: '『 هذا الامر مخصص للمشرفين فقط 』',
+    botAdmin: '『 يجب رفع البوت ل ادمن 』',
     premime: '『 ميزات خاصة متميزة إذا كنت ترغب في تسجيل نوع الإيجار 』',
-    owner: '『 الميزه دي للمطور بس! 』',
-    group: '『 الميزه دي للجروبات بس! 』',
+    owner: '『 هذه الميزة للمطور فقط! 』',
+    group: '『 هذه الميزة للمطور! 』',
     private: '『 الميزه دي في الخاص بس! 』',
-    bot: '『 الميزه دي للبوت بس! 』',
-    wait: '『 استني شويه... 』',
+    bot: '『 هذه الميزة للبوت فقط! 』',
+    wait: '『 انتظر ثواني... 』',
     linkm: '『 فين الرابط? 』',
     endLimit: 'انتهت صلاحية الحد اليومي الخاص بك ، وسيتم إعادة تعيين الحد كل 12 ساعة',
     nsfw: 'لم يتم تنشيط ميزة nsfw ، يرجى الاتصال بالمسؤول للتفعيل',
